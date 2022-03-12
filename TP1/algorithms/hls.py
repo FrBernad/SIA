05e8c9993f33e67353011665c41db3dc95b3769e
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2022-03-12 17:51:46 -0300

    feat: add hls

--- TP1/algorithms/hls.py
+++ TP1/algorithms/hls.py
@@ -1,8 +1,20 @@
 from collections import Iterable
 
+from sortedcontainers import SortedList
+
+from algorithms.config import Config
+from algorithms.stats import Stats
 from utils.board import State
-from utils.node import Node
+from utils.node import HeuristicNode
 
 
-def hls(init_state: State) -> Iterable[Node]:
-    return
+def hls(init_state: State, stats: Stats, config: Config) -> Iterable[HeuristicNode]:
+    border = SortedList([HeuristicNode(init_state, None, config.heuristic)])
+
+    while border:
+        current_node = border.pop(0)
+
+        if current_node.is_objective():
+            return current_node.get_tree()
+
+        border.append(current_node.get_child_nodes())
