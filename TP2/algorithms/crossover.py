abb03442c7cfa28b373c98ae93ca050472563009
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2022-03-26 15:46:40 -0300

    feat: add uniform crossover

--- TP2/algorithms/crossover.py
+++ TP2/algorithms/crossover.py
@@ -1,7 +1,9 @@
-from random import sample
+from random import sample, random
 from typing import Tuple
 
 from utils.backpack import Chromosome
+
+UNIFORM_PROBABILITY = 0.5
 
 
 def simple_crossover(couple: Tuple[Chromosome, Chromosome]) -> Tuple[Chromosome, Chromosome]:
@@ -21,6 +23,17 @@ def multiple_crossover(
     for i in range(len(crossover_points)):
         if i % 2 != 0:
             swap_elements(crossover_points[i - 1], crossover_points[i], s1, s2)
+
+    return tuple(s1), tuple(s2)
+
+
+def uniform_crossover(couple: Tuple[Chromosome, Chromosome]) -> Tuple[Chromosome, Chromosome]:
+    s1 = list(couple[0])
+    s2 = list(couple[1])
+
+    for i in range(len(s1)):
+        if random() < UNIFORM_PROBABILITY:
+            s1[i], s2[i] = s2[i], s1[i]
 
     return tuple(s1), tuple(s2)
 
