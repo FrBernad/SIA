37da818daa72995597d409215766def74ca65d11
Joaco Lega <jlegammare@itba.edu.ar>
2022-04-03 16:33:59 -0300

    fix: couple selection graphs

--- TP2/utils/graphs.py
+++ TP2/utils/graphs.py
@@ -461,6 +461,64 @@ def _generate_selection_plots(chromosome_factory):
         fig.show()
 
 
+def _generate_couple_plots(chromosome_factory, initial_population_size, mutation_probability):
+    first_generation = chromosome_factory.generate_random_population(initial_population_size)
+    cross_over_method = CROSSOVER_METHODS.get('simple_crossover')
+    figures = []
+
+    for selection_method in SELECTION_METHODS.items():
+        fig = []
+        for couple_selection_method in COUPLE_SELECTION_METHODS.items():
+            print(f'\tGenerating {selection_method} {couple_selection_method[1]} plot')
+
+            end_condition_config = EndConditionConfig(
+                generations_count=2000,
+                time=5 * 60,
+                acceptable_solution_generation_count=1000,
+                structure_consecutive_generations=30,
+                percentage=0.7,
+                fitness_consecutive_generations=30,
+                fitness_min_generations=1000,
+                structure_min_generations=1000
+            )
+            config = Config({}, initial_population_size, end_condition_config,
+                            benefit_weight_ratio, couple_selection_method[1],
+                            CrossoverMethodConfig(cross_over_method, n=4),
+                            MutationMethodConfig(random_mutation, probability=mutation_probability),
+                            SelectionMethodConfig(selection_method[1], threshold=0.7, k=0.5, T0=200, Tc=10,
+                                                  truncation_size=initial_population_size - 2))
+            stats = genetic_algorithm(first_generation, chromosome_factory, config.couple_selection_method,
+                                      config.crossover_method_config.method, config.mutation_method_config.method,
+                                      config.selection_method_config.method, config)
+
+            x = range(0, config.end_condition_config.acceptable_solution_generation_count + 20)
+            figures.append(
+                go.Scatter(
+                    name=f'{couple_selection_method[0].replace("_", " ").title()}',
+                    x=list(x),
+                    y=list(map(lambda c: c.fitness, stats.best_solutions)),
+                ),
+            )
+
+        fig = go.Figure(
+            figures,
+            {
+                'title': f'{selection_method[0].replace("_", " ").title()}',
+                'xaxis_title': "Generation",
+                'yaxis_title': "Fitness",
+                'legend': dict(
+                    bgcolor='rgba(0,0,0,0)',
+                    y=0,
+                    x=0.7
+                )
+            }
+        )
+
+        fig.update_xaxes(type="log")
+        fig.show()
+        figures.clear()
+
+
 if __name__ == '__main__':
     INITIAL_POPULATION_SIZES = [100]
     MUTATION_PROBABILITY = [0.005]
@@ -472,13 +530,10 @@ if __name__ == '__main__':
     #
     # print('Generating Boltzmann plots')
     # _generate_boltzmann_plots(chromosome_factory, 100, 0.005)
-    #
-    # print('Generating Population Plots')
-    # _generate_population_plots(chromosome_factory)
-    #
-    # print('Generating Tournament plots')
-    # _generate_tournament_plots(chromosome_factory, 100, 0.005)
-    #
+
+    # print('Generating Truncated plots')
+    # _generate_truncated_plots(chromosome_factory, 100, 0.005)
+
     # print('Generating overlapped plots')
     # _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['simple_crossover', simple_crossover])
     # _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['multiple_crossover', multiple_crossover])
@@ -491,5 +546,5 @@ if __name__ == '__main__':
     # print('Generating Population plots')
     # _generate_population_plots(chromosome_factory, [10, 50, 100], 0.005)
 
-    # print('Generating Couple Selection plots')
-    # _generate_population_plots(chromosome_factory, 100, 0.005)
+    print('Generating Couple Selection plots')
+    _generate_couple_plots(chromosome_factory, 100, 0.005)
