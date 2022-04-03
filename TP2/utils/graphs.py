80783277431157c55c6d90ce0aca45d133382fc4
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2022-04-03 16:17:06 -0300

    feat: add population graphs

--- TP2/utils/graphs.py
+++ TP2/utils/graphs.py
@@ -338,6 +338,68 @@ def _generate_mutation_plots(chromosome_factory):
         fig.show()
 
 
+def _generate_population_plots(chromosome_factory):
+    initial_population_sizes = [10, 50, 100]
+
+    couple_selection_method = ['rand_couple_selection', rand_couple_selection]
+    crossover_method = ["simple_crossover", simple_crossover]
+
+    for selection_method in SELECTION_METHODS.items():
+        figures = []
+        for initial_population in initial_population_sizes:
+            print(f'\tGenerating {selection_method[0]} {initial_population} plot')
+
+            end_condition_config = EndConditionConfig(
+                generations_count=2000,
+                time=5 * 60,
+                acceptable_solution_generation_count=1000,
+                structure_consecutive_generations=30,
+                percentage=0.7,
+                fitness_consecutive_generations=30,
+                fitness_min_generations=1000,
+                structure_min_generations=1000
+            )
+
+            config = Config({}, initial_population, end_condition_config,
+                            benefit_weight_ratio, couple_selection_method[1],
+                            CrossoverMethodConfig(crossover_method[1], n=4),
+                            MutationMethodConfig(random_mutation, probability=MUTATION_PROBABILITY[0]),
+                            SelectionMethodConfig(selection_method[1], threshold=0.7, k=0.5, T0=200, Tc=10,
+                                                  truncation_size=initial_population - 2))
+
+            first_generation = chromosome_factory.generate_random_population(initial_population)
+
+            stats = genetic_algorithm(first_generation, chromosome_factory, config.couple_selection_method,
+                                      config.crossover_method_config.method, config.mutation_method_config.method,
+                                      config.selection_method_config.method, config)
+
+            x = range(0, config.end_condition_config.acceptable_solution_generation_count + 20)
+            figures.append(
+                go.Scatter(
+                    name=f'{initial_population}',
+                    x=list(x),
+                    y=list(map(lambda c: c.fitness, stats.best_solutions)),
+                ),
+            )
+
+        fig = go.Figure(
+                figures,
+                {
+                    'title': f'{selection_method[0].replace("_", " ").title()}',
+                    'xaxis_title': "Generation",
+                    'yaxis_title': "Fitness",
+                    'legend': dict(
+                        bgcolor='rgba(0,0,0,0)',
+                        y=0,
+                        x=0.8
+                    )
+                }
+            )
+
+        fig.update_xaxes(type="log")
+        fig.show()
+
+
 def _generate_selection_plots(chromosome_factory):
     initial_population_size = 100
 
@@ -405,25 +467,26 @@ if __name__ == '__main__':
 
     chromosome_factory = ChromosomeFactory(_get_knapsack_data("../" + DEFAULT_DATA_FILE), benefit_weight_ratio)
 
-    print('Generating selection plots')
-    _generate_selection_plots(chromosome_factory)
+    # print('Generating selection plots')
+    # _generate_selection_plots(chromosome_factory)
+    #
+    # print('Generating Boltzmann plots')
+    # _generate_boltzmann_plots(chromosome_factory, 100, 0.005)
+    #
+    # print('Generating Population Plots')
+    # _generate_population_plots(chromosome_factory)
+    #
+    # print('Generating Tournament plots')
+    # _generate_tournament_plots(chromosome_factory, 100, 0.005)
+    #
+    # print('Generating overlapped plots')
+    # _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['simple_crossover', simple_crossover])
+    # _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['multiple_crossover', multiple_crossover])
+    # _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['uniform_crossover', uniform_crossover])
 
-    print('Generating Boltzmann plots')
-    _generate_boltzmann_plots(chromosome_factory, 100, 0.005)
+    #print('Generating Population plots')
+    #_generate_population_plots(chromosome_factory)
 
-    print('Generating Truncated plots')
-    _generate_truncated_plots(chromosome_factory, 100, 0.005)
-
-    print('Generating Tournament plots')
-    _generate_tournament_plots(chromosome_factory, 100, 0.005)
-
-    print('Generating overlapped plots')
-    _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['simple_crossover', simple_crossover])
-    _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['multiple_crossover', multiple_crossover])
-    _generate_overlapped_plots(chromosome_factory, 100, 0.005, ['uniform_crossover', uniform_crossover])
-
-    print('Generating Mutation plots')
-    _generate_mutation_plots(chromosome_factory)
     #
     # print('Generating Population plots')
     # _generate_population_plots(chromosome_factory, [10, 50, 100], 0.005)
